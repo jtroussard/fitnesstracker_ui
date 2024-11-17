7aa10fe0bf4e6a32acf82f70d4dc18fa3623523a
Jacques Troussard <mm_sparrow@beeswaxnoneofyours.com>
2024-11-17 15:41:07 -0500

    layout-redesign : [fix] update import paths in logout button

--- src/components/auth/LogoutButton.js
+++ src/components/auth/LogoutButton.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';  // Import AuthContext
-import MainButton from './MainButton';
+import MainButton from '../common/controls/MainButton';
 
 const Logout = () => {
   const { handleLogout } = useContext(AuthContext);  // Use AuthContext
